b14ebba91e96b75e0972b97f949280876f315062
Kimmo Puputti <kpuputti@gmail.com>
2012-10-09 19:08:31 +0300

    Redirect all requests from www.kpuputti.fi to kpuputti.fi.

--- web.js
+++ web.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+// Redirect all www.kpuputti.fi to kpuputti.fi
+app.all('*', function (request, response, next) {
+    var host = request.headers.host;
+
+    if (host === 'www.kpuputti.fi') {
+        response.redirect(301, 'http://kpuputti.fi' + request.url);
+    } else {
+        next();
+    }
+});
+
 app.get('/', function (request, response) {
     response.sendfile('public/index.html');
 });
